11ea9340df80f9d836530e3045220d75fd46c66d
gigamegawatts@gmail.com <gigamegawatts@gmail.com@431c876a-a7a2-df64-f014-8552a661e052>
2011-10-29 18:18:56 +0000

    .Net Micro Framework Source code for "FEZ XBee Sensor" blog post

---
+++ trunk/XBeeSensor/XBeeSensor/clsLCD_SFKit.cs
@@ -0,0 +1,225 @@
+using System;
+using Microsoft.SPOT;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace XBeeSensor
+{
+    public class clsLCD_SFKit : clsLCD_SF
+    {
+
+        private byte[] readBuffer = new byte[100];
+        private int intBytesRead = 0;
+        private int intBufferOffset = 0;
+        const byte CMD_SPECIAL = 0xFE;
+        public event CommandEventHandler Command;
+
+        public clsLCD_SFKit(string strPort, Int16 intBaud, byte rows, byte cols)
+        {
+            lcd = new SerialPort(strPort, intBaud, Parity.None, 8, StopBits.One);
+            lcd.Open();
+            SetScreenSize(rows, cols);
+  
+            strBlankRow = new string(' ', cols);
+            lcd.DataReceived += new SerialDataReceivedEventHandler(lcd_DataReceived);
+        }
+
+        void lcd_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                // read the data
+                intBytesRead = lcd.Read(readBuffer, intBufferOffset, lcd.BytesToRead);
+                Debug.Print("in lcd_DataReceived: " + intBytesRead);
+                if (intBytesRead == 0 || readBuffer[intBufferOffset + intBytesRead - 1] != 0x0a)
+                {
+                    // only got a partial command, so append whatever comes next to this partial command
+                    intBufferOffset += intBytesRead;
+                    return;
+                }
+
+                // ensure there is an ending 0x00 for the string
+                readBuffer[intBufferOffset + intBytesRead] = 0x00;
+                String strInData = new String(Encoding.UTF8.GetChars(readBuffer));
+                intBufferOffset = 0;
+                Debug.Print("Got data: " + strInData);
+
+                // if there is more than 1 command, each one is terminated by cr/lf
+
+                string[] strCommands = strInData.Split(new char[] { (char)10, (char)13 });
+
+                for (int i = 0; i < strCommands.Length; i++)
+                {
+                    if (strCommands[i] != "") // ignore empty string between CR and LF
+                    {
+                        // if it looks like a command, tell rest of application that it was received
+                        if (strCommands[i][1] == ':')
+                        {
+                            raiseCommand(strCommands[i]);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Print("Exception in lcd_DataReceived: " + ex.Message);
+                Debug.Assert(false);
+            }
+        }
+
+        /// <summary>
+        /// Raise event telling rest of program that a command was received over the serial port
+        /// </summary>
+        /// <param name="strCommand">Command text (format is x:[xxxx...])</param>
+        /// <returns>true if command was handled by one of the event recipients</returns>
+        private bool raiseCommand(string strCommand)
+        {
+            if (Command == null)
+            {
+                // there are no event handlers out there
+                return false;
+            }
+            CommandArgs args = new CommandArgs(strCommand);
+            // raise event using Command delegate
+            Command(this, args);
+            return args.blnHandled;
+        }
+
+        new public void SelectLine(byte intLine, bool blnClearLine)
+        {
+
+            byte[] command = new byte[3];
+            command[0] = CMD_SPECIAL;
+            command[1] = 0x80;
+            command[2] = (byte)((intLine - 1) * numCols);
+
+
+            lcd.Write(command, 0, command.Length);
+
+
+            if (blnClearLine)
+            {
+                Thread.Sleep(10); // let LCD catch up
+                WriteStringToLCD(strBlankRow);
+                Thread.Sleep(10); // let LCD catch up
+                lcd.Write(command, 0, command.Length);
+            }
+            
+        }
+
+        new public void SetBrightness(byte bytBrightness)
+        {
+            try
+            {
+                byte[] command = new byte[2];
+                command[0] = 0x80;
+                command[1] = bytBrightness;
+                lcd.Write(command, 0, command.Length);
+
+            }
+            catch (Exception ex)
+            {
+                // catch and release
+                Debug.Assert(false);
+                Debug.Print("Exception in SetBrightness: " + ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Set LCD dimensions
+        /// </summary>
+        /// <param name="lcdRows"></param>
+        /// <param name="lcdCols"></param>
+        new public void SetScreenSize(byte lcdRows, byte lcdCols)
+        {
+            numRows = lcdRows;
+            numCols = lcdCols;
+
+            byte[] command = new byte[2];
+            command[0] = CMD_SPECIAL;
+
+            // set num rows
+            lcd.Write(command, 0, command.Length);
+            if (lcdRows == 4)
+            {
+                command[1] = 5;
+            }
+            else
+            {
+                // 2 rows - only other setting supported
+                command[1] = 6;
+            }
+            lcd.Write(command, 0, command.Length);
+            Thread.Sleep(10); // let LCD catch up
+
+            // set num cols
+            if (lcdCols == 20)
+            {
+                command[1] = 3;
+            }
+            else
+            {
+                // 16 cols - only other settings supported
+                command[1] = 4;
+            }
+            lcd.Write(command, 0, command.Length);
+
+        }
+
+
+        new public void SetCursorPos(byte row, byte col)
+        {
+            byte[] command = new byte[3];
+            command[0] = CMD_SPECIAL;
+            command[1] = 0x80;
+            command[2] = (byte)((row - 1) * numCols + col  - 1);
+
+
+            lcd.Write(command, 0, command.Length);
+        }
+
+
+        public void RequestSettings()
+        {
+            byte[] command = new byte[2];
+            command[0] = CMD_SPECIAL;
+            command[1] = 70;
+            lcd.Write(command, 0, command.Length);
+        }
+
+
+        public void RequestHumidityAndTemp()
+        {
+            byte[] command = new byte[2];
+            command[0] = CMD_SPECIAL;
+            command[1] = 71;
+            lcd.Write(command, 0, command.Length);
+        }
+
+        public void RequestSetting(byte address)
+        {
+            byte[] command = new byte[3];
+            command[0] = CMD_SPECIAL;
+            command[1] = 72;
+            command[2] = address;
+            lcd.Write(command, 0, command.Length);
+           
+        }
+
+        public void SaveSetting(byte address, byte setting)
+        {
+            byte[] command = new byte[4];
+            command[0] = CMD_SPECIAL;
+            command[1] = 73;
+            command[2] = address;
+            command[3] = setting;
+            lcd.Write(command, 0, command.Length);
+        }
+    }
+}
---
+++ trunk/XBeeSensor/XBeeSensor/clsUART.cs
@@ -0,0 +1,304 @@
+using System;
+using System.Threading;
+using System.IO.Ports;
+using System.Text;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+
+namespace XBeeSensor
+{
+    public class clsUART
+    {
+        // NOTE: SerialPort requires that you add Microsoft.Spot.Hardware.SerialPort to the Project's References list
+         private SerialPort UART = null;
+         private int read_count = 0;
+         private byte[] readBuffer;
+         //private byte[] writeBuffer;
+         private int intBufferLen = 0; // length of read buffer
+         private int intBufferIndex = 0;
+         private bool blnSendResponse;
+         private string strPortName;
+         private Int16 intBaud;
+         public event CommandEventHandler Command;
+
+        public clsUART(string strPort, Int16 intBaud, int intReadBufferLen, bool blnSendResponse)
+        {
+            intBufferLen = intReadBufferLen;
+            readBuffer = new byte[intReadBufferLen];
+            this.blnSendResponse = blnSendResponse;
+            this.strPortName = strPort;
+            this.intBaud = intBaud;
+            UART = new SerialPort(strPort, intBaud, Parity.None, 8, StopBits.One);
+            UART.Open();
+            UART.DataReceived += new SerialDataReceivedEventHandler(UART_DataReceived);
+            Debug.Print(strPort + " Opened at baud " + intBaud);
+            //WriteToUART("Hello World");
+        }
+
+        public void WriteToUART(String strData)
+        {
+            WriteToUART(strData, true);
+        }
+
+        public  void WriteToUART(String strData, bool blnSendCRLF)
+        {
+            if (blnSendCRLF)
+            {
+                strData += "\r\n";
+            }
+
+            byte[] buffer = Encoding.UTF8.GetBytes(strData);
+            UART.Write(buffer, 0, buffer.Length);
+        }
+
+
+
+        private void UART_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                //do
+                //{
+                    // read the data
+                    read_count = UART.Read(readBuffer, intBufferIndex, UART.BytesToRead);
+
+                    // did we get an ending null?
+                    if (read_count > 0 && readBuffer[intBufferIndex + read_count - 1] == 0)
+                    {
+                        intBufferIndex = 0;
+                        String strCommand = new String(Encoding.UTF8.GetChars(readBuffer));
+                        processCommand(strCommand);
+                    }
+                    else
+                    {
+                        // set the buffer pointer so that the next data read is concatenated to the end
+                        intBufferIndex += read_count;
+                        //Thread.Sleep(1000); // pause to see if rest of command comes in
+                    }
+               // } while (UART.BytesToRead > 0);
+                Debug.Assert(intBufferIndex < 20, "How do we get incomplete data?");
+
+            }
+            catch (Exception ex)
+            {
+                //Debug.Assert(false);
+                Debug.Print(ex.Message);
+            }
+        }
+
+        private bool processCommand(string strCommand)
+        {
+            try
+            {
+                Debug.Print("in processCommand, got " + strCommand);
+                strCommand = strCommand.Trim();
+                if (strCommand[1] != ':')
+                {
+                    if (blnSendResponse)
+                    {
+                        WriteToUART("?");
+                    }
+                    return false;
+                }
+                char command = strCommand[0];
+                switch (command)
+                {
+                    case 'T':
+
+                        // format must be hh:mm[:ss]
+                        string[] strTimeParts = strCommand.Substring(2).Split(new char[] { ':', ' ', '.', '-' });
+                        if (strTimeParts.Length >= 2)
+                        {
+                            DateTime currentDayTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day,
+                                Int16.Parse(strTimeParts[0]), Int16.Parse(strTimeParts[1]), 0);
+                            if (strTimeParts.Length >= 3)
+                            {
+                                currentDayTime.AddSeconds(Int16.Parse(strTimeParts[1]));
+                            }
+                            Utility.SetLocalTime(currentDayTime);
+                            Debug.Print("time is now " + DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second);
+
+                            // tell rest of application that the time has been set
+                            raiseCommand(strCommand); 
+
+                            if (blnSendResponse)
+                            {
+                                WriteToUART("OK");
+                            }
+                            return true;
+
+                        }
+                        break;
+
+                    case 'D':
+                        // format must be yy-mm-dd or yyyy-mm-dd or yyyy-mm-dd hh:mm
+                        string[] strDateParts = strCommand.Substring(2).Split(new char[] { ':', ' ', '.', '-' });
+                        if (strDateParts.Length >= 3)
+                        {
+                            Int16 intYear = Int16.Parse(strDateParts[0]);
+                            // if yy, convert to yyyy
+                            if (intYear < 100)
+                            {
+                                intYear += 2000;
+                            }
+
+                            int intHours = DateTime.Now.Hour;
+                            int intMinutes = DateTime.Now.Minute;
+                            int intSeconds = DateTime.Now.Second;
+                            bool blnTimeSet = false;
+                            if (strDateParts.Length == 5)
+                            {
+                                intHours = Int32.Parse(strDateParts[3]);
+                                intMinutes = Int32.Parse(strDateParts[4]);
+                                intSeconds = 0;
+                                blnTimeSet = true;
+                            }
+                           
+                            DateTime currentDayTime = new DateTime(intYear, 
+                                Int16.Parse(strDateParts[1]), Int16.Parse(strDateParts[2]), intHours,
+                                intMinutes, intSeconds);
+
+
+                            Utility.SetLocalTime(currentDayTime);                            
+                            Debug.Print("date is now " + DateTime.Now.Year + "-" + DateTime.Now.Month + ":" + DateTime.Now.Day);
+
+                            // tell rest of application that the date has been set
+                            raiseCommand(strCommand);
+                            if (blnTimeSet)
+                            {
+                                // raise the time event too
+                                raiseCommand("T:" + Program.Int_ToZeroPrefixedString(intHours, 2) + ":" + Program.Int_ToZeroPrefixedString(intMinutes, 2));
+                            }
+
+                            if (blnSendResponse)
+                            {
+                                WriteToUART("OK");
+                            }
+                            return true;
+                        }
+
+                        break;
+
+                    default:
+                        // looks like a command - maybe some other part of the program knows how to handle it?
+                        //CommandArgs args = new CommandArgs(strCommand);
+                        //Command(this, args);
+                        //if (args.blnHandled)
+                        if (raiseCommand(strCommand))
+                        {
+                            WriteToUART("OK");
+                            return true;
+                        }
+                        break;
+                }
+                if (blnSendResponse)
+                {
+                    WriteToUART("?");
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                //Debug.Assert(false);
+                Debug.Print(ex.Message);
+                if (blnSendResponse)
+                {
+                    WriteToUART("?");
+                }
+                return false;
+            }
+        }
+
+        public bool ClosePort()
+        {
+            if (IsOpen())
+            {
+                UART.Close();
+                UART.Dispose();
+                UART = null;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ReopenPort()
+        {
+            try
+            {
+                if (!IsOpen())
+                {
+                    UART = new SerialPort(strPortName, intBaud, Parity.None, 8, StopBits.One);
+                    UART.Open();
+                    UART.DataReceived += new SerialDataReceivedEventHandler(UART_DataReceived);
+                    Debug.Print("COM port reopened");
+                    WriteToUART("Hello again World");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                Debug.Assert(false);
+                Debug.Print(ex.Message);
+                return false;
+            }
+        }
+
+        public bool IsOpen()
+        {
+            if (UART != null && UART.IsOpen)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Raise event telling rest of program that a command was received over the serial port
+        /// </summary>
+        /// <param name="strCommand">Command text (format is x:[xxxx...])</param>
+        /// <returns>true if command was handled by one of the event recipients</returns>
+        private bool raiseCommand(string strCommand)
+        {
+            if (Command == null)
+            {
+                // there are no event handlers out there
+                return false;
+            }
+            CommandArgs args = new CommandArgs(strCommand);
+            // raise event using Command delegate
+            Command(this, args);
+            return args.blnHandled;
+        }
+
+    }
+
+    
+
+
+    public class CommandArgs : EventArgs
+    {
+        public string StrCommand { get; set; }
+
+        public CommandArgs(string strCommand)
+        {
+            this.StrCommand = strCommand;
+        }
+
+        /// <summary>
+        /// if true, command was handled by the event handler
+        /// </summary>
+        public bool blnHandled;
+    }
+}
